9cf9b54aba3234b34d762d177fd110509c649855
Dinakar04 <sathyamoorthydinakar@gmail.com>
2023-08-21 19:12:53 +0530

    amazone clone with home ,product

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import "./Product.css";
 
 
-function Product() {
+function Product(id,title,details,price,rating,image) {
   return (
-    <h1>Product</h1>
+    <div className="product__container">
+      {/* <!-- product --> */}
+      <div className="product__info">
+        <small>{title}</small>
+        <p>{details}</p>
+        
+      </div>
+      <div className="product__price">
+        <p>$<b>{price}</b></p>
+      </div>
+      <div className='product__img'> 
+      <img src={image} alt="" />
+      </div>
+
+      <button className="cart__button">Add to Cart</button>
+
+
+    </div>
   )
 }
 
